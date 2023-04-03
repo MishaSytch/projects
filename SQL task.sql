022ee15b89cbb47ac5fa9c087b26a9345cd9451b
Hans Christopher Linchpin <misha.sytch@mail.ru>
2023-04-03 18:46:12 +0300

    Не решена 3-я задача

--- SQL task.sql
+++ SQL task.sql
@@ -2,6 +2,12 @@
 select NAME, max(SALARY) as SALARY
 from EMPLOYEE;
 
+-- или
+
+select NAME
+from EMPLOYEE
+where SALARY = (select max(SALARY) from EMPLOYEE);
+
 
 --Отдел, с максимальной суммарной зарплатой сотрудников.
 select DEPARTMENT_ID, SUM(SALARY) as Summary
@@ -10,11 +16,17 @@ join DEPARTMENT D on D.ID = EMPLOYEE.DEPARTMENT_ID
 group by D.ID, D.NAME
 order by Summary desc limit 1;
 
+-- или
+
+select DEPARTMENT_ID, sum(SALARY) as sum_salary
+from EMPLOYEE
+group by DEPARTMENT_ID
+order by sum_salary desc limit 1;
 
 --Вывести одно число: максимальную длину цепочки руководителей по таблице сотрудников (вычислить глубину дерева).
-select
-    count(*) as Summary
-from EMPLOYEE
+
+
+
 
 
 -- Сотрудника, чье имя начинается на «Р» и заканчивается на «н».
